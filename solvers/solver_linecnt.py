179c6af5fb70d782857249c0d6b4179e991df4e0
kazutosh <75659655+kazutosh@users.noreply.github.com>
2024-04-20 09:47:47 +0900

    [solver_linecnt] fix: リファクタリングして、コメントを追加した

--- solvers/solver_linecnt.py
+++ solvers/solver_linecnt.py
@@ -9,11 +9,12 @@ class Act:
         self.y = y
         self.s = s
 
+# 解答を保持する配列
 answer = list[Act]()
 
+# 問題を読み込む
 with open(sys.argv[1]) as f:
     problem = json.loads(f.read())
-
 board = problem["board"]
 width = board["width"]
 height = board["height"]
@@ -21,23 +22,18 @@ start = [[int(c) for c in line] for line in board["start"]]
 goal = [[int(c) for c in line] for line in board["goal"]]
 
 
-# それぞれの行の整数値の数を揃える
-# 上の行から揃えていく
+# 行の整数値の数を保持する配列
 goal_cnt = list[dict[int, int]]()
 start_cnt = list[dict[int, int]]()
-
-for i in range(height):
+for y in range(height):
     goal_cnt.append({c: 0 for c in range(4)})
     start_cnt.append({c: 0 for c in range(4)})
+    for x in range(width):
+        goal_cnt[y][goal[y][x]] += 1
+        start_cnt[y][start[y][x]] += 1
 
-for i, line in enumerate(goal):
-    for c in line:
-        goal_cnt[i][c] += 1
-
-for i, line in enumerate(start):
-    for c in line:
-        start_cnt[i][c] += 1
-
+# それぞれの行の整数値の数を揃える
+# 上の行から揃えていく
 for y in range(height):
     for x in range(width):
         c = start[y][x]
@@ -70,9 +66,11 @@ for y in range(height):
                 if nx >= 0 and nx < width and ny >= 0 and ny < height:
                     q.put((nx, ny))
         
+        # 交換対象が見つからなかったらスキップ
         if swapx == -1:
             continue
 
+        # 抜き型を実際に適用して解答を作る
         # 交換対象が元のセルより右側にある場合
         if x < swapx:
             # 左づめだから2
@@ -83,19 +81,17 @@ for y in range(height):
             answer.extend([Act(0, px, swapy, 3) for px in range(swapx, x)])
             start[swapy][swapx:x + 1] = reversed(start[swapy][swapx:x + 1])
         
+        # 上詰めだから0
         answer.extend([Act(0, x, py, 0) for py in reversed(range(y, swapy))])
         for i in range((swapy - y + 2) // 2):
             start[y + i][x], start[swapy - i][x] = start[swapy - i][x], start[y + i][x]
         
         # 整数値の個数を再計算する
-        start_cnt = list[dict[int, int]]()
-
-        for i in range(height):
+        start_cnt.clear()
+        for sy in range(height):
             start_cnt.append({c: 0 for c in range(4)})
-
-        for i, line in enumerate(start):
-            for c in line:
-                start_cnt[i][c] += 1
+            for sx in range(width):
+                start_cnt[sy][start[sy][sx]] += 1
 
 # 行ごとに復元する
 for y in range(height):
@@ -103,6 +99,7 @@ for y in range(height):
         if goal[y][x] == start[y][x]:
             continue
         
+        # 自分より右のセルから交換対象のセルを見つける
         for sx in range(x + 1, width):
             # 交換先のセルを見つけたなら
             if start[y][sx] == goal[y][x]:
