09dd346f2fec4227d4acb34fe38bfe88bb2c5848
m22092 <105482183+m22092@users.noreply.github.com>
2024-10-19 09:10:15 +0900

    テスターの追加

---
+++ solvers/tester.py
@@ -0,0 +1,28 @@
+import subprocess
+import numpy as np
+import json
+import glob
+import tempfile
+
+files = glob.glob("../problems/64_64*.json")
+print(files)
+for x in files:
+    print(x)
+    cp1 = subprocess.run(["beam/build/solver_simple",x], text=True, capture_output=True)
+    # cp1.wait()
+    try:
+        out = json.loads(cp1.stdout)
+        print(out["n"])
+        temp_file_path = tempfile.mktemp()
+        problem_json = open(temp_file_path, 'w')
+        json.dump(out, problem_json)
+        problem_json.close()
+        chack = subprocess.run(["python3","check_answer.py",x,temp_file_path], text=True, capture_output=True)
+        print(chack.stdout,end="")
+        if chack.stdout != "correct\n":
+            print(cp1.stderr)
+        # pass
+    except:
+        print("ソルバーで問題が発生しました", 500)
+        raise TypeError("ソルバーで問題が発生しました")
+        # return resp
