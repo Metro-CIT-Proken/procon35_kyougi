020150fd72c7e9c612f36810375750ac3f0403ec
m22092 <105482183+m22092@users.noreply.github.com>
2024-10-19 10:19:09 +0900

    ソルバーの指定可能

--- solvers/tester.py
+++ solvers/tester.py
@@ -7,17 +7,23 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--filed', type=str,default="")
+parser.add_argument('-s', '--solver', type=str,default="solver_simple")
 args = parser.parse_args()
 
 files = glob.glob(f"../problems/{args.filed}*.json")
 print(files)
+count_all = 0
 for x in files:
     print(x)
-    cp1 = subprocess.run(["beam/build/solver_simple",x], text=True, capture_output=True)
+    cp1 = subprocess.run([f"beam/build/{args.solver}",x], text=True, capture_output=True)
     # cp1.wait()
     try:
+        print(cp1.stdout)
+        print(cp1.stderr)
+
         out = json.loads(cp1.stdout)
         print(out["n"])
+        count_all += out["n"]
         temp_file_path = tempfile.mktemp()
         problem_json = open(temp_file_path, 'w')
         json.dump(out, problem_json)
@@ -31,3 +37,4 @@ for x in files:
         print("ソルバーで問題が発生しました", 500)
         raise TypeError("ソルバーで問題が発生しました")
         # return resp
+print("averege = ",count_all/len(files))
